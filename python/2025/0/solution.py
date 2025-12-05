bcf73e3a1927956187a4a37e4073af91b0fed166
dimitrilahaye <contact@dimitrilahaye.net>
2025-12-05 07:51:12 +0100

    fix: test config

---
+++ python/2025/0/solution.py
@@ -0,0 +1,25 @@
+from utils.file_reader import read_input
+
+YEAR = 2025
+DAY = 0
+
+
+def part1(data: list[str]) -> int:
+    return data.__len__()
+
+
+def part2(data: list[str]) -> int:
+    return data.__len__()
+
+
+def solve():
+    input_data = read_input(DAY, YEAR)
+    answer_part1 = part1(input_data)
+    answer_part2 = part2(input_data)
+    print(f"--- Advent of Code {YEAR} - Jour {DAY:02d} ---")
+    print(f"Partie 1: {answer_part1}")
+    print(f"Partie 2: {answer_part2}")
+
+
+if __name__ == "__main__":
+    solve()
---
+++ python/2025/0/test_0.py
@@ -0,0 +1,28 @@
+import pytest
+import os
+from solution import part1, part2
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+def test_part1():
+    # Arrange
+    data: list[str] = ["1", "2"]
+
+    # Act
+    result = part1(data)
+
+    # Assert
+    assert result == 2
+
+
+@pytest.mark.skip()
+def test_part2():
+    # Arrange
+    data: list[str] = ["1", "2"]
+
+    # Act
+    result = part2(data)
+
+    # Assert
+    assert result == 2
